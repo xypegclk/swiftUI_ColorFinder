542a0e7fafd692ce41c87bb5a4babe661cc9ff68
xypegclk <xypagclg@gmail.com>
2020-05-01 11:31:37 +0800

    complete

--- swiftUI_ColorFinder/ContentView.swift
+++ swiftUI_ColorFinder/ContentView.swift
@@ -9,13 +9,50 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State var redValue: Double
+    @State var greenValue: Double
+    @State var blueValue: Double
+    
     var body: some View {
-        Text("Hello World")
+        ZStack {
+            Color(red: redValue, green: greenValue, blue: blueValue)
+                .edgesIgnoringSafeArea(.all)
+            VStack {
+                HStack {
+                    MyText(color: "R", value: redValue)
+                    Spacer()
+                    MyText(color: "G", value: greenValue)
+                    Spacer()
+                    MyText(color: "B", value: blueValue)
+                }.offset(x: 0, y: 50)
+                
+                Spacer()
+                
+                Slider(value: $redValue)
+                Slider(value: $greenValue)
+                Slider(value: $blueValue)
+            }.frame(width: UIScreen.main.bounds.size.width * 0.7)
+        }
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        ContentView(redValue: 1.0, greenValue: 1.0, blueValue: 1.0)
+    }
+}
+
+struct MyText: View {
+    var color: String
+    var value: Double
+    
+    var body: some View {
+        HStack {
+            Text(color + ": \(Int(value * 255.0))")
+                .font(.title)
+                .fontWeight(.light)
+            Spacer()
+        }.frame(width: 100.0).fixedSize()
     }
 }
