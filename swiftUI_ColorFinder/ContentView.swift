ef548ed358e30af95ef42247c417ab2d1bb36075
xypegclk <xypagclg@gmail.com>
2020-05-01 10:59:39 +0800

    Initial Commit

---
+++ swiftUI_ColorFinder/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  swiftUI_ColorFinder
+//
+//  Created by 薛義郎 on 2020/5/1.
+//  Copyright © 2020 薛義郎. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
